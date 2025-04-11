34322a158e4f3268bd95c8b0c18b87b025c27be2
acidphantasm <acidphantasm@acidphantasm.com>
2025-04-11 17:30:34 -0500

    Use Player spawn points & PMC spawn system

--- Client/Patches/BossSpawnPatches.cs
+++ Client/Patches/BossSpawnPatches.cs
@@ -1,4 +1,5 @@
-﻿using acidphantasm_botplacementsystem.Utils;
+﻿using acidphantasm_botplacementsystem.Spawning;
+using acidphantasm_botplacementsystem.Utils;
 using EFT;
 using EFT.Game.Spawning;
 using HarmonyLib;
@@ -8,12 +9,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 namespace acidphantasm_botplacementsystem.Patches
 {
     internal class PMCWaveCountPatch : ModulePatch
     {
-        public static int _BossWaveCount = 0;
         protected override MethodBase GetTargetMethod()
         {
             return AccessTools.Method(typeof(BossSpawnScenario), nameof(BossSpawnScenario.method_0));
@@ -23,11 +24,13 @@ namespace acidphantasm_botplacementsystem.Patches
         private static void PatchPostfix(BossSpawnScenario __instance, BossLocationSpawn[] bossWaves)
         {
             if (__instance == null) return;
-            List<BossLocationSpawn> pmcWaveCount = bossWaves.Where(x => x.BossName == "pmcUSEC" || x.BossName == "pmcBEAR").ToList();
-            _BossWaveCount = pmcWaveCount.Count;
+
             Utility.currentMapZones = new List<BotZone>();
             Utility.mapName = string.Empty;
             Utility.allPMCs = new List<IPlayer>();
+            Utility.allBots = new List<IPlayer>();
+            Utility.allSpawnPoints = new List<ISpawnPoint>();
+            Utility.playerSpawnPoints = new List<ISpawnPoint>();
         }
     }
     internal class PMCDistancePatch : ModulePatch
@@ -50,68 +53,56 @@ namespace acidphantasm_botplacementsystem.Patches
                 return true;
             }
 
-            if (Utility.currentMapZones.Count == 0)
-            {
-                Utility.currentMapZones = wave.GetPossibleZones(__instance.botZone_0, __instance.botZone_0.ToList()).Where(x => !x.SnipeZone).ToList();
-            }
+            Logger.LogInfo($"Spawn Point Attempt: {creationData.Profiles[0].Nickname} | WildSpawnType: {wave.BossType} | Count: {1 + wave.EscortCount}");
 
-            List<BotZone> botZones = Utility.GetMapBotZones();
+            int soloPointCount = 1;
+            int escortPointCount = 1 + wave.EscortCount;
+
             List<IPlayer> pmcList = Utility.GetAllPMCs();
             List<IPlayer> scavList = Utility.GetAllScavs();
+            string location = Utility.GetCurrentLocation() ?? "default";
+            float distance = GetDistanceForMap(location);
+            List<ISpawnPoint> validSpawnLocations = GetValidSpawnPoints(pmcList, scavList, distance, escortPointCount);
+            var botZone = __instance.botSpawner_0.GetClosestZone(validSpawnLocations[0].Position, out float _);
 
-            Logger.LogInfo($"Spawn Attempt: {creationData.Profiles[0].Nickname} | WildSpawnType: {wave.BossType} | Count: {1 + wave.EscortCount}");
-
-            for (int i = 0; i < botZones.Count; i++)
+            if (validSpawnLocations.Count >= soloPointCount)
             {
-                BotZone botZone = botZones[i];
-
-                int soloPointCount = 1;
-                int escortPointCount = 1 + wave.EscortCount;
-
-                string location = Utility.GetCurrentLocation() ?? "default";
-                float distance = GetDistanceForMap(location);
-                List<ISpawnPoint> validSpawnLocations = GetValidSpawnPoints(botZone, location, pmcList, scavList, distance, escortPointCount);
-                if (validSpawnLocations.Count >= soloPointCount)
+                if (validSpawnLocations.Count < escortPointCount && validSpawnLocations.Count > 0)
                 {
-                    if (validSpawnLocations.Count < escortPointCount && validSpawnLocations.Count > 0)
+                    int neededSpawnPointCount = escortPointCount - validSpawnLocations.Count;
+                    ISpawnPoint spawnPoint = validSpawnLocations[0];
+                    for (int i = 0; i < neededSpawnPointCount; i++)
                     {
-                        int num4 = escortPointCount - validSpawnLocations.Count;
-                        ISpawnPoint spawnPoint = validSpawnLocations[0];
-                        for (int j = 0; j < num4; j++)
-                        {
-                            validSpawnLocations.Add(spawnPoint);
-                        }
+                        validSpawnLocations.Add(spawnPoint);
                     }
-                    if (validSpawnLocations.Count >= escortPointCount)
+                }
+                if (validSpawnLocations.Count >= escortPointCount)
+                {
+                    __instance.float_1 = Time.time;
+                    __instance.wildSpawnType_0 = wave.BossType;
+                    __instance.botZone_1 = botZone;
+                    if (creationData.SpawnStopped)
                     {
-                        __instance.float_1 = Time.time;
-                        __instance.wildSpawnType_0 = wave.BossType;
-                        __instance.botZone_1 = botZone;
-
-                        if (creationData.SpawnStopped)
-                        {
-                            __result = false;
-                            return false;
-                        }
-                        __instance.method_3(creationData, wave, spawnParams, followersCount, botZone, validSpawnLocations);
-                        __result = true;
+                        __result = false;
                         return false;
                     }
+                    //__instance.method_3(creationData, wave, spawnParams, followersCount, botZone, validSpawnLocations);
+                    PMCSpawning.StartSpawnPMCGroup(creationData, wave, spawnParams, followersCount, botZone, validSpawnLocations, __instance, __instance.botSpawner_0, __instance.iBotCreator);
+                    __result = true;
+                    return false;
                 }
             }
 
-            Logger.LogInfo($"No valid spawnpoints outside defined distance, consider adjusting limits: {creationData.Profiles[0].Nickname} | WildSpawnType: {wave.BossType} | Count: {1 + wave.EscortCount}");
+            Logger.LogInfo($"No valid spawnpoints found - spawning anywhere: {creationData.Profiles[0].Nickname} | WildSpawnType: {wave.BossType} | Count: {1 + wave.EscortCount}");
 
             return true;
         }
-        private static List<ISpawnPoint> GetValidSpawnPoints(BotZone botZone, string location, IReadOnlyCollection<IPlayer> pmcPlayers, IReadOnlyCollection<IPlayer> scavPlayers, float distance, int neededPoints)
+
+        private static List<ISpawnPoint> GetValidSpawnPoints(IReadOnlyCollection<IPlayer> pmcPlayers, IReadOnlyCollection<IPlayer> scavPlayers, float distance, int neededPoints)
         {
             List<ISpawnPoint> validSpawnPoints = new List<ISpawnPoint>();
-            location = location.ToLower();
 
-            List<ISpawnPoint> list = botZone.SpawnPoints.ToList()
-                .Where(x => !x.Categories.ContainBossCategory() || x.Categories == ESpawnCategoryMask.All)
-                .ToList();
+            List<ISpawnPoint> list = Utility.GetPlayerSpawnPoints();
 
             list = list.OrderBy(_ => Guid.NewGuid()).ToList();
 
@@ -120,20 +111,19 @@ namespace acidphantasm_botplacementsystem.Patches
             for (int i = 0; i < list.Count; i++)
             {
                 ISpawnPoint checkPoint = list[i];
-                if (!foundInitialPoint && IsValid(checkPoint, pmcPlayers, distance) && IsValid(checkPoint, scavPlayers, 25f))
-                {
-                    validSpawnPoints.Add(checkPoint);
-                    foundInitialPoint = true;
-                    neededPoints++;
-                }
-                if (foundInitialPoint && Vector3.Distance(checkPoint.Position, validSpawnPoints[0].Position) <= 15f)
-                {
-                    validSpawnPoints.Add(checkPoint);
-                    neededPoints++;
-                }
+
                 if (validSpawnPoints.Count == neededPoints)
                 {
                     return validSpawnPoints;
+                }
+                if (foundInitialPoint && Vector3.Distance(checkPoint.Position, validSpawnPoints[0].Position) <= 20f)
+                {
+                    validSpawnPoints.Add(checkPoint);
+                }
+                if (!foundInitialPoint && IsValid(checkPoint, pmcPlayers, distance))
+                {
+                    validSpawnPoints.Add(checkPoint);
+                    foundInitialPoint = true;
                 }
             }
             return validSpawnPoints;
