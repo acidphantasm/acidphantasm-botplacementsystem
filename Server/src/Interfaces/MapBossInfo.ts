f5f346641388dfefe289b3ea9ea318dd72e1c2bd
acidphantasm <acidphantasm@acidphantasm.com>
2025-04-15 23:46:14 -0500

    Remove unneeded files

--- Server/src/Interfaces/MapBossInfo.ts
+++
@@ -1,6 +0,0 @@
-import { MinMax } from "@spt/models/common/MinMax"
-import { IBossLocationSpawn } from "@spt/models/eft/common/ILocationBase";
-
-export interface LocationInformation {
-    locations: Record<string, IBossLocationSpawn>
-}
